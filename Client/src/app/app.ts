2a6e32c5d700cd3cee166254ca7e5377c6c400aa
rahulsahay19 <rahulsahay19@hotmail.com>
2025-10-18 01:44:51 +0530

    Module Completed

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from "./core/navbar/navbar";
 import { RouterOutlet } from '@angular/router';
 import { Loading } from './core/loading/loading';
+import { ChatBotComponent } from './core/chatbot/chatbot';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Loading],
+  imports: [RouterOutlet, Navbar, Loading, ChatBotComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
